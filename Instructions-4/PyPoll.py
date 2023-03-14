f8e755b088d4601c6de0ceb2f8139b6de1d94cab
clambach <125232781+clambach@users.noreply.github.com>
2023-03-14 00:04:46 -0400

    Update PyPoll.py

--- Instructions-4/PyPoll.py
+++ Instructions-4/PyPoll.py
@@ -10,7 +10,7 @@ unique = []
 candidates = []
 
 #Pull the csv file
-csvpath = 'PyPoll/Resources/election_data.csv'
+csvpath = 'Instructions-4/PyPoll/Resources/election_data.csv'
 
 #open csv
 with open(csvpath) as csvfile:
@@ -22,60 +22,6 @@ with open(csvpath) as csvfile:
      count.append(row[0])
      unique.append(row[2])
     candidates = [*set(unique)]
-    # per.append(sum(row[2])/count)
-
-from collections import Counter
- 
-def winner(input):
- 
-    # convert list of candidates into dictionary
-    # output will be likes candidates = {'A':2, 'B':4}
-    votes = Counter(input)
-     
-    # create another dictionary and it's key will
-    # be count of votes values will be name of
-    # candidates
-    dict = {}
- 
-    for value in votes.values():
- 
-        # initialize empty list to each key to
-        # insert candidate names having same
-        # number of votes
-        dict[value] = []
- 
-    for (key,value) in votes.items():
-        dict[value].append(key)
- 
-    # sort keys in descending order to get maximum
-    # value of votes
-    maxVote = sorted(dict.keys(),reverse=True)[0]
- 
-    # check if more than 1 candidates have same
-    # number of votes. If yes, then sort the list
-    # first and print first element
-    if len(dict[maxVote])>1:
-        print (sorted(dict[maxVote])[0])
-    else:
-        print (dict[maxVote][0])
- 
-# Driver program
-if __name__ == "__main__":
-    input =['john','johnny','jackie','johnny',
-            'john','jackie','jamie','jamie',
-            'john','johnny','jamie','johnny',
-            'john']
-    winner(input)
-
-
-
-    
-    #  loss.append(int(row[1]))
-
-    #  #total votes
-    # for i in range(len(loss)-1):
-    #     change.append(loss[i-1]-loss[i])
-
 
     #print queries
     print(" ")
@@ -97,11 +43,4 @@ if __name__ == "__main__":
        new.write("------------------- ")
        new.write("\n")
        new.write(f"Total Votes:  {len(count)}")
-       new.write("\n")
-       new.write(f"")
-       new.write("\n")
-       new.write(f"")
-       new.write("\n")
-       new.write(f"")
-       new.write("\n")
-       new.write(f"")
+       new.write(x)
